93cb9174a7b213222f3fab2fee570d977d1f6549
wizounovziki <42036658+wizounovziki@users.noreply.github.com>
2021-11-07 16:01:37 +0800

    Update main.py

--- SystemCode/backend/main.py
+++ SystemCode/backend/main.py
@@ -21,4 +21,4 @@ api.add_resource(Rating,'/rating')
 
 api.add_resource(Calculate_movie_mean,"/movie/management/itisdangerous")
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=4397)
+    app.run(host='0.0.0.0',port=5001)
